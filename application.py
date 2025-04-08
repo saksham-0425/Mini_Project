2c38dc2e963fc14798fb64b1687fb0a9e95b0f31
Saksham Gupta <guptasaksham2510@gmail.com>
2025-04-08 12:41:03 +0530

    Update application.py

--- application.py
+++ application.py
@@ -111,6 +111,7 @@ def inject_custom_style():
     }
 
     .movie-card {
+        display:none;
         background: var(--surface);
         border-radius: 16px;
         border: 1px solid var(--border);
